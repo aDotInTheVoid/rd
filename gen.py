f441361e33f18cfcd06df1e88b1b85b3d728bd7c
Nixon Enraght-Moony <nixon.emoony@gmail.com>
2020-11-19 15:18:27 +0000

    More stuff

--- gen.py
+++ gen.py
@@ -18,54 +18,164 @@
 # https://this-week-in-rust.org/
 # https://timetill.rs/#/
 # https://rust-lang.github.io/rustup-components-history/
-# https://rust-fuzz.github.io/book/introduction.html
-# https://doc.rust-lang.org/rust-by-example/index.html
-# https://lib.rs/
-# https://docs.rs/
-# https://crates.io/
-# https://rust-lang.github.io/chalk/book/
-# https://rust-lang.github.io/rustup/
-# https://rustc-dev-guide.rust-lang.org/
-# https://doc.rust-lang.org/nightly/reference/
-# https://doc.rust-lang.org/nomicon/
-# https://doc.rust-lang.org/stable/edition-guide/
-# https://rust-lang.github.io/unsafe-code-guidelines
-# https://rfcbot.rs/
-# https://rust-lang.github.io/rfcs/
-# https://thanks.rust-lang.org/
-# https://rust-lang.github.io/api-guidelines/
-# https://blog.rust-lang.org/
-# https://blog.rust-lang.org/inside-rust/index.html
-# https://doc.rust-lang.org/rustc/
-# https://doc.rust-lang.org/cargo/
-# https://doc.rust-lang.org/unstable-book/
-# https://rust-lang.github.io/rustfmt/?version=master&search=
 
 content = [
+    {
+        "header": "The Rust Fuzzing Book",
+        "content": "Describes how to fuzz code written in rust",
+        "link": "https://rust-fuzz.github.io/book/",
+        "linkname": "Read the fuzzing book",
+    },
+    {
+        "header": "Rust by Example",
+        "content": "Rust by Example is a collection of runnable examples that illustrate various Rust concepts and standard libraries.",
+        "link": "https://doc.rust-lang.org/rust-by-example/",
+        "linkname": "Check out Rust by Example",
+    },
+    {
+        "header": "lib.rs",
+        "content": "Fast, lightweight, opinionated, unofficial alternative to crates.io.",
+        "link": "https://lib.rs/",
+        "linkname": "Check out lib.rs",
+    },
+    {
+        "header": "docs.rs",
+        "content": "Docs.rs hosts documentation for all the crates on crates.io.",
+        "link": "https://docs.rs/",
+        "linkname": "Check out crate docs",
+    },
+    {
+        "header": "crates.io",
+        "content": "crates.io is the Rust community's central package registry that serves as a location to discover and download packages.",
+        "link": "https://crates.io/",
+        "linkname": "Check out crates.io",
+    },
+    {
+        "header": "Chalk",
+        "content": "Chalk is a library that implements the Rust trait system. The implementation is meant to be practical and usable, but also high-level enough to map easily to a full specification. It is also meant to be an independent library that can be integrated both into the main rustc compiler and also other programs and contexts.",
+        "link": "https://rust-lang.github.io/chalk/book/",
+        "linkname": "Read the chalk docs",
+    },
+    {
+        "header": "Rustup documentation",
+        "content": "rustup installs The Rust Programming Language from the official release channels, enabling you to easily switch between stable, beta, and nightly compilers and keep them updated.",
+        "link": "https://rust-lang.github.io/rustup/",
+        "linkname": "Read the rustup docs",
+    },
+    {
+        "header": "Rustc dev guide",
+        "content": "This guide is meant to help document how rustc – the Rust compiler – works, as well as to help new contributors get involved in rustc development.",
+        "link": "https://rustc-dev-guide.rust-lang.org/",
+        "linkname": "Read the dev guide",
+    },
+    {
+        "header": "The Reference",
+        "content": "It describes each language construct and their use, memory model, concurrency model, runtime services, linkage model, and debugging facilities",
+        "link": "https://doc.rust-lang.org/reference/",
+        "linkname": "Read the reference",
+    },
+    {
+        "header": "The Rustonomicon",
+        "content": """THE KNOWLEDGE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF UNLEASHING INDESCRIBABLE HORRORS THAT SHATTER YOUR PSYCHE AND SET YOUR MIND ADRIFT IN THE UNKNOWABLY INFINITE COSMOS.""",
+        "link": "https://doc.rust-lang.org/nomicon/",
+        "linkname": "Dig into all the awful details",
+    },
+    {
+        "header": "Edition Guide",
+        "content": """"Editions" are Rust's way of communicating large changes in the way that it feels to write Rust code. This guide contains the content of editions, as well as migration instructions""",
+        "link": "https://doc.rust-lang.org/stable/edition-guide/",
+        "linkname": "Read the edition guide",
+    },
+    {
+        "header": "Unsafe Code Guidelines",
+        "content": """This document is produced by the UCG WG to provide a "guide" for writing unsafe code that "recommends" what kinds of things unsafe code can and cannot do, and that documents which guarantees unsafe code may rely on. It is largely a work-in-progress right now.""",
+        "link": "https://rust-lang.github.io/unsafe-code-guidelines",
+        "linkname": "Read the guidelines",
+    },
+    {
+        "header": "Active RFCs",
+        "content": "These are nominations to stabilize something",
+        "link": "https://rfcbot.rs/",
+        "linkname": "See active RFCs",
+    },
+    {
+        "header": "Adopted RFCs",
+        "content": "These are the RFCs that have been approved",
+        "link": "https://rust-lang.github.io/rfcs/",
+        "linkname": "Read the RFCs",
+    },
+    {
+        "header": "thanks.rust-lang.org",
+        "content": "Thanks is a place for us to give thanks to everyone who contributes to the Rust project.",
+        "link": "https://thanks.rust-lang.org/",
+        "linkname": "Be thankfull",
+    },
+    {
+        "header": "Rust API Guidelines",
+        "content": "This is a set of recommendations on how to design and present APIs for the Rust programming language. They are authored largely by the Rust library team, based on experiences building the Rust standard library and other crates in the Rust ecosystem.",
+        "link": "https://rust-lang.github.io/api-guidelines/",
+        "linkname": "Read the api guidelines",
+    },
+    {
+        "header": "Rust Blog",
+        "content": "This blog is used for anouncements that effect all rust users, like releases and important projects",
+        "link": "https://blog.rust-lang.org/",
+        "linkname": "Read the rust blog",
+    },
+    {
+        "header": "Inside Rust Blog",
+        "content": "This blog is aimed at those who wish to follow along with Rust development. The various Rust teams and working groups use this blog to post status updates, calls for help, and other similar announcements.",
+        "link": "https://blog.rust-lang.org/inside-rust/",
+        "linkname": "Read the inside rust blog",
+    },
+    {
+        "header": "The Rustc Book",
+        "content": "Rustc is the rust compiller. Most Rust programmers don't invoke rustc directly, but instead do it through Cargo. But it can still be good to know how it works",
+        "link": "https://doc.rust-lang.org/rustc/",
+        "linkname": "Read the rustc book",
+    },
+    {
+        "header": "The Cargo Book",
+        "content": "Cargo is the Rust package manager. It Cargo downloads your Rust package’s dependencies, compiles your packages, makes distributable packages, and uploads them to crates.io",
+        "link": "https://doc.rust-lang.org/cargo/",
+        "linkname": "Read the cargo book",
+    },
+    {
+        "header": "Unstable book",
+        "content": "This book has documentation of the nighly features that haven't been stabilized yet",
+        "link": "https://doc.rust-lang.org/unstable-book/",
+        "linkname": "Read the Unstable book",
+    },
+    {
+        "header": "Rustfmt",
+        "content": "Rustfmt is the code formatter for rust",
+        "link": " https://rust-lang.github.io/rustfmt/?version=master&search=",
+        "linkname": "Rustfmt configuration",
+    },
     {
         "header": "The Standard Library",
         "content": "Rust's standard library has extensive API documentation, with explanations of how to use various things, as well as example code for accomplishing various tasks.",
         "link": "https://doc.rust-lang.org/stable/std/",
-        "linkname": "Check out the standard library"
+        "linkname": "Check out the standard library",
     },
     {
         "header": "The Book",
         "content": """Affectionately nicknamed "the book," The Rust Programming Language will give you an overview of the language from first principles. You'll build a few projects along the way, and by the end, you'll have a solid grasp of the language.""",
         "linkname": "Read the book",
-        "link": "https://doc.rust-lang.org/stable/book/index.html"
+        "link": "https://doc.rust-lang.org/stable/book/index.html",
     },
     {
-        "header": "Error listing", 
+        "header": "Error listing",
         "content": "Many of Rust's errors come with error codes, and you can request extended diagnostics from the compiler on those errors. You can also read them online, if you prefer to read them that way.",
         "link": "https://doc.rust-lang.org/stable/error-index.html",
-        "linkname": "See the errors"
+        "linkname": "See the errors",
     },
     {
         "header": "Bors Queue",
         "content": "Bors is rust's CI bot, that keeps master green. The queue has all the PRs that have been approved and are waiting to be merged",
         "link": "https://bors.rust-lang.org/queue/all",
-        "linkname": "See whats coming"
-    }
+        "linkname": "See whats coming",
+    },
 ]
 
 
